191030a65a0e9d1fd5dc7a0858581315b33e894c
u4bi <rootcode10@gmail.com>
2019-08-27 03:10:21 +0900

    feat: Add morgan for development.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import express from 'express'
+import morgan from 'morgan'
 
 const app: express.Application = express()
+
+const env: string = process.env.NODE_ENV || 'development'
+
+if (env === 'development') app.use(morgan('dev'))
 
 export default app
